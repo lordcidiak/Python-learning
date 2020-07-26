a9280d5b44024afc6375a98c67f7b9404505c082
juan pablo sanchez gaitan <50275221+lordcidiak@users.noreply.github.com>
2020-07-25 19:05:57 -0500

    Update juego_de_la_vida.py

--- juego_de_la_vida.py
+++ juego_de_la_vida.py
@@ -3,6 +3,7 @@
 #autor juan pablo sanchez 
 import pygame
 import numpy as np
+import time
 
 pygame.init()
 #ancho y alto de la pantalla
@@ -20,10 +21,25 @@ dimCH = height / nyC
 
 gameState = np.zeros((nxC, nyC))
 
+gameState[5,3] = 1
+gameState[5,4] = 1
+gameState[5,5] = 1
+
+gameState[21,21] = 1
+gameState[22,22] = 1
+gameState[22,23] = 1
+gameState[21,23] = 1
+gameState[20,23] = 1
+
+
 
 #bucle de ejecucion
 
 while True:
+    newGameState = np.copy(gameState)
+
+    screen.fill(bg)
+    time.sleep(0.1)
 
     for y in range(0, nxC):
         for x in range(0, nyC):
@@ -35,22 +51,27 @@ while True:
                       gameState[(x + 1) % nxC, (y)     % nyC] + \
                       gameState[(x - 1) % nxC, (y + 1) % nyC] + \
                       gameState[(x)     % nxC, (y + 1) % nyC] + \
-                      gameState[(x + 1) % nxC, (y + 1) % nyC] + \
+                      gameState[(x + 1) % nxC, (y + 1) % nyC]
 
 
-                if gameState[x ,y ] == 0 and n_neigh == 3:
-                    gameState[x, y] = 1
+            if gameState[x, y] == 0 and n_neigh == 3:
+                    newGameState[x, y] = 1
 
-                elif gameState [x, y] == 1 and (n_neigh < 2 or n_neigh > 3);
-                    gameState[x, y] = 0
- \
+            elif gameState [x, y] == 1 and (n_neigh < 2 or n_neigh > 3):
+                    newGameState[x, y] = 0
+
                 #se crea el poligono de cada celda a dibujar
             poly = [((x) * dimCW,   y * dimCH),
                         ((x+1) * dimCW, y * dimCH),
                         ((x+1) * dimCW, (y+1) * dimCH),
                         ((x) * dimCW,(y+1) * dimCH)]
 
+            if newGameState [x, y] == 0:
+                 pygame.draw.polygon(screen, (128,128,128), poly, 1)
+            else:
+                 pygame.draw.polygon(screen, (255,255,255), poly, 0)
 
-            pygame.draw.polygon(screen, (128,128,128), poly, 1)
+    gameState = np.copy(newGameState)
+
     pygame.display.flip()
 
